37a886bc86d8ae117eb012299415a4e0390eccd1
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-08-11 11:46:01 +0530

    index update

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const containerEl = document.quarySlector(".container")
 
-containerEl.innerHTML = "jfhidfshi"
+const careers = ["youTuber","web Developer","Freelancer","Instructor"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText()
+
+function updateText(){
+containerEl.innerHTML = `
+    <h1>I am a ${careers[careerIndex]}</h1>
+`;
+}
