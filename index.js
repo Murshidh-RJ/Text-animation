5797fde05aac19c537165e7fcfdee5d8c9d33d27
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-08-11 13:30:55 +0530

    final comit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const containerEl = document.quarySlector(".container")
+const containerEl = document.querySelector(".container");
 
-const careers = ["youTuber","web Developer","Freelancer","Instructor"];
+const careers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
 
 let careerIndex = 0;
 
 let characterIndex = 0;
 
-updateText()
+updateText();
 
-function updateText(){
-containerEl.innerHTML = `
-    <h1>I am a ${careers[careerIndex]}</h1>
-`;
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>
+    `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
 }
